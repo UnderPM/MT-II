3c70485a3562a76e351beeac4158a8229d732c12
UnderPM <UnderPM@users.noreply.github.com>
2017-03-08 11:46:21 -0800

    Added some menus and a function to get pi; Added geometry class

--- MT II/MT II/MT II.cpp
+++ MT II/MT II/MT II.cpp
@@ -1,30 +1,69 @@
 #include <iostream>
 #include <math.h>
 
+float getPi();
+void mainMenu();
+void runFunctionsMenu();
+
+class Geometry {
+public:
+	float AngleRad(float deg_angle)const {
+		if (deg_angle > 0) {
+			deg_angle = 360 - deg_angle;
+		}
+		deg_angle * getPi() / 180;
+	}
+	float GetDisplacedVolume()const {
+		return displacement / cylinders;
+	}
+	float GetCombChamberVolume()const {
+		return GetDisplacedVolume() / (compressionRatio - 1);
+	}
+
+	Geometry(const float disp_in, const int cyl_in, const float rod_in, const float cr_in, const int underSq_Sq_OverSq) {
+		displacement = disp_in;
+		cylinders = cyl_in;
+		rodLength = rod_in;
+		compressionRatio = cr_in;
+		enum square_t {UNDER_SQ = -1, SQUARE = 0, OVER_SQ = 1};
+		switch (underSq_Sq_OverSq)
+		{
+		case UNDER_SQ:
+			isSquare = false;
+			isUnderSquare = true;
+			isOverSquare = false;
+			break;
+		case SQUARE:
+			isSquare = true;
+			isUnderSquare = false;
+			isOverSquare = false;
+			break;
+		case OVER_SQ:
+			isSquare = false;
+			isUnderSquare = false;
+			isOverSquare = true;
+			break;
+		}
+	}
+
+private:
+	float displacement;
+	int cylinders;
+	float compressionRatio;
+	float bore;
+	float stroke;
+	float rodLength;
+	bool isSquare = false;
+	bool isUnderSquare = false;
+	bool isOverSquare = false;
+};
+
 int main() {
 	using namespace std;
-	
-	const float pi = 4 * atan(1.0);
-	static constexpr float third = 1.0 / 3.0;
-	
-	float displacement = 3.0e-3f;
-	int cylinders = 6;
-	
+
 	int rpm = 3600;
 	int rps = rpm / 60;
 
-	float compressionRatio = 9.5f;
-	float displacedVolume = displacement / cylinders;
-	float combustionVolume = displacedVolume / (compressionRatio - 1);
-	float volumeAtAngle = 0.0f;
-	
-	int intAngle = 0;
-	float angleDeg = 20.0f;
-	float angleRad = angleDeg * pi / 180;
-
-	float bore = 0.0f;
-	float stroke = 0.0f;
-	float rodLength = 16.6e-2f;
 	//float boreStrokeRatio = bore / stroke;
 
 	float avgPistonSpeed = 0.0f;
@@ -32,43 +71,11 @@ int main() {
 	float position = 0.0f;
 	float delta_pos = 0.0f;
 	
-	bool isSquare = true;
-	bool isUnderSquare = false;
-	bool isOverSquare = false;
 
 	//main loop here
-	//main menu
-	cout << "MT-II" << endl;
-	cout << "1 - Run Functions" << endl;
-	cout << "2 - Exit" << endl;
-	enum option_t {RUN_FUNCTIONS = 1, EXIT = 2};
-	int option;
-	cin >> option;
+	
 
-	switch (option)
-	{
-	case RUN_FUNCTIONS:
-		//call secondary menu
-		//test start
-		if (isSquare) {
-			bore = pow(float(4 * displacedVolume / pi),third);
-			stroke = bore;
-			cout << "bore" << endl << bore << endl;
-		}
-		avgPistonSpeed = 2 * stroke * rps;
-		pistonSpeed = (pi / 2.0f) * sin(angleRad) * (1 + (cos(angleRad) / sqrt(pow(2 * rodLength / stroke, 2.0f) -
-			pow(sin(angleRad), 2.0f)))) * avgPistonSpeed;
-		position = (stroke / 2.0f) * cos(angleRad) + sqrt(pow(rodLength, 2.0f) - pow(stroke / 2.0f, 2.0f) * 
-			pow(sin(angleRad), 2.0f));
-		delta_pos = rodLength + (stroke / 2.0f) - position;
-		volumeAtAngle = combustionVolume + (pi * pow(bore, 2.0f) / 4) * (rodLength + (stroke / 2.0f) - position);
-		break;
-	case EXIT:
-		return 0;
-		break;
-	default:
-		break;
-	}
+	
 	/*
 	if (boreStrokeRatio > 1) {
 		isOverSquare = true;
@@ -88,3 +95,83 @@ int main() {
 	*/
 	
 }
+
+float getPi() {
+	return 4 * atan(1.0);
+}
+
+void mainMenu() {
+	using namespace std;
+	
+	bool isFinished = false;
+	do {
+		cout << "MT-II" << endl;
+		cout << "1 - Run Functions" << endl;
+		cout << "2 - Exit" << endl;
+		enum option_t { RUN_FUNCTIONS = 1, EXIT = 2 };
+		int option;
+		cin >> option;
+
+		switch (option)
+		{
+		case RUN_FUNCTIONS:
+			//call secondary menu
+
+			break;
+		case EXIT:
+			isFinished = true;
+			break;
+		default:
+			break;
+		}
+	} while (!isFinished);
+}
+
+void runFunctionsMenu() {
+	using namespace std;
+	bool isFinished = false;
+
+	do {
+
+		cout << "1 - Geometry" << endl;
+		cout << "2 - Cinematic" << endl;
+		enum option_t { GEOMETRIC = 1, CINEMATIC = 2, BACK = 3 };
+		int option;
+		cin >> option;
+
+		switch (option) {
+		case GEOMETRIC:
+			// call geometricMenu
+			break;
+		case CINEMATIC:
+			// call cinematicMenu
+			break;
+		case BACK:
+			isFinished = true;
+			break;
+		default:
+			break;
+		}
+	} while (!isFinished);
+}
+
+void geometricMenu() {
+	using namespace std;
+	bool isFinished = false;
+}
+
+
+
+/*//test start
+if (isSquare) {
+bore = cbrt(4.0 * displacedVolume / getPi());
+stroke = bore;
+cout << "bore" << endl << bore << endl;
+}
+avgPistonSpeed = 2 * stroke * rps;
+pistonSpeed = (getPi() / 2.0f) * sin(angleRad) * (1 + (cos(angleRad) / sqrt(pow(2 * rodLength / stroke, 2.0f) -
+pow(sin(angleRad), 2.0f)))) * avgPistonSpeed;
+position = (stroke / 2.0f) * cos(angleRad) + sqrt(pow(rodLength, 2.0f) - pow(stroke / 2.0f, 2.0f) *
+pow(sin(angleRad), 2.0f));
+delta_pos = rodLength + (stroke / 2.0f) - position;
+volumeAtAngle = combustionVolume + (getPi() * pow(bore, 2.0f) / 4) * (rodLength + (stroke / 2.0f) - position);*/
